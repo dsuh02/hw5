1e518279d7dd3020d95e0705ba4a2bd0b351d72a
Daniel Suh <dsuh8205@usc.edu>
2023-04-13 00:33:02 +0000

    all done!

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,8 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool createSchedule(size_t hor, size_t ver, size_t list, size_t required, vector<size_t> move, 
+	            const AvailabilityMatrix& avail, DailySchedule& sched);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -36,10 +37,46 @@ bool schedule(
         return false;
     }
     sched.clear();
+
     // Add your code below
 
+    vector<size_t> move(avail[0].size());fill(move.begin(),move.end(),0);
 
+    // make sched
 
-
+    vector<Worker_T> workersVect(dailyNeed);
+	DailySchedule curr(avail.size(), workersVect);
+    bool fin =createSchedule(size_t{},size_t{},maxShifts,dailyNeed,move,avail,curr);
+	sched =curr;
+	return fin;
 }
 
+bool createSchedule(size_t hor, size_t ver, size_t list, size_t required, vector<size_t> move, 
+	            const AvailabilityMatrix& avail, DailySchedule& sched){
+	if(avail.size()==hor) {
+        return true;
+    }
+    else{
+        bool found = false;
+        for (size_t j=0; avail[0].size()>j; j++) {
+            if (avail[hor][j]==1 && list!=move[j]) {
+                sched[hor][ver]=j;
+                move[j]=move[j]+1;
+                
+                const size_t vertical_count=sched[0].size();
+                size_t clength=(ver+1)%vertical_count;
+                size_t hlength =hor+(ver+1)/vertical_count;
+                found =createSchedule(hlength,clength,list,required,move,avail,sched);
+                if(found){
+                    return true;
+                }
+                else{
+                    move[j]--;
+                    sched[hor][ver]=0;
+                }
+            }
+        }
+        return found;
+    }
+    return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,11 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void wordleHelper(string in, const string& floating, 
+    const set<string>& dict, set<string>& results, 
+    unsigned int index); 
+
+bool validWord(string in, const string& floating);
 
 
 // Definition of primary wordle function
@@ -22,7 +27,61 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    std::set<std::string> results;
+    std::string incopy = in;
+    std::string fcopy = floating;
+    wordleHelper(incopy, fcopy, dict, results, 0);
+    return results;
 }
 
 // Define any helper functions here
+// recursed function in wordle function
+void wordleHelper(string in, const string& floating, const set<string>& dict, set<string>& results, unsigned int index)
+{
+    //1st case word is not valid
+    if(!validWord(in,floating)){
+        return;
+    }
+    //if index is at the last position
+    if(index==in.length()){
+        if(dict.find(in) != dict.end() && validWord(in, floating)){
+            results.insert(in);
+        }
+        return;
+    }
+    // if the next position is a blank
+    if(in[index]=='-'){
+        for(char c='a'; c <= 'z'; c++){ //travel through alphabet ASCII
+            in[index]=c;
+            //recurse for every possible letter for the next position
+            wordleHelper(in, floating, dict, results, index+1);
+        }
+    }
+    //else recurse with next position
+    else{
+        wordleHelper(in, floating, dict, results, index+1);
+    }
+}
+
+//returns true if there are NO MORE floating positions
+//false if floating var has a '-' position
+bool validWord(string in, const string& floating)
+{
+    if(floating == "")
+        return true;
+    for(unsigned int i=0; i<floating.length(); i++)
+    { 
+        if(in.find(floating[i]) == string::npos && in.find('-')==string::npos){
+            return false;
+        }
+        else{
+            if(in.find(floating[i]) != std::string::npos){
+                in.erase(in.find(floating[i]), 1);
+            }
+            else{
+                in.erase(in.find('-'), 1);
+            }
+        }
+    }
+    return true;
+}
